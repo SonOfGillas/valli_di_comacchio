ea768ba7a5411596b78c25770869567290621809
Riccardo Gardenghi <riccardo.gardenghi@gellify.it>
2025-06-28 15:52:28 +0200

    initial setup of npc_reset_data.dart

--- lib/app/shared/domain/data_sources/npc_data_source/npc_reset_data.dart
+++ lib/app/shared/domain/data_sources/npc_data_source/npc_reset_data.dart
@@ -5,37 +5,45 @@
 // al carpone
 // qua qua
 
+import 'dart:math';
+
 import 'package:valli_di_comacchio/app/shared/domain/entities/npc.dart';
 
-const List<Npc> npcResetData = [
+/// Generate a random wealth value for NPCs.
+/// the value should be between 3000 and 20000
+int generateRandomWealth() {
+  return Random().nextInt(17000) + 3000;
+}
+
+List<Npc> npcResetData = [
   Npc(
     id: 'npc_1',
     name: 'Rosario',
-    imageName: 'npc_1.png',
-    locationName: 'Porto Garibaldi',
-    longitude: 12.2500,
-    latitude: 44.7000,
-    wealth: 100,
+    imageName: 'rosario.png',
+    locationName: 'Via dei Bilancioni',
+    longitude: 12.197045,
+    latitude: 44.672905,
+    wealth: generateRandomWealth(),
     inventory: [],
   ),
   Npc(
     id: 'npc_2',
     name: 'Eelena',
-    imageName: 'npc_2.png',
-    locationName: 'Comacchio',
-    longitude: 12.2000,
-    latitude: 44.7000,
-    wealth: 150,
+    imageName: 'eelena.png',
+    locationName: 'Ponte dei Trepponti',
+    longitude: 12.183259,
+    latitude: 44.693005,
+    wealth: generateRandomWealth(),
     inventory: [],
   ),
   Npc(
     id: 'npc_3',
     name: 'Pino',
-    imageName: 'npc_3.png',
+    imageName: 'pino.png',
     locationName: 'Lido di Spina',
-    longitude: 12.3000,
-    latitude: 44.7000,
-    wealth: 200,
+    longitude: 12.252110,
+    latitude: 44.653382,
+    wealth: generateRandomWealth(),
     inventory: [],
   ),
   Npc(
@@ -43,9 +51,19 @@ const List<Npc> npcResetData = [
     name: 'Al Carpone',
     imageName: 'npc_4.png',
     locationName: 'Lido degli Estensi',
-    longitude: 12.3500,
-    latitude: 44.7000,
-    wealth: 250,
+    longitude: 12.247454,
+    latitude: 44.669262,
+    wealth: generateRandomWealth(),
+    inventory: [],
+  ),
+  Npc(
+    id: 'npc_5',
+    name: 'Qua Qua',
+    imageName: 'npc_5.png',
+    locationName: 'Porto Garibaldi',
+    longitude: 12.243106,
+    latitude: 44.678690,
+    wealth: generateRandomWealth(),
     inventory: [],
   ),
 ];
