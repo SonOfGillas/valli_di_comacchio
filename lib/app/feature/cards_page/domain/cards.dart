d2b5ac41804048baf247196659353b5f75e24467
Riccardo Gardenghi <riccardo.gardenghi@gellify.it>
2025-07-01 11:20:47 +0200

    incrise cards luminosity

--- lib/app/feature/cards_page/domain/cards.dart
+++ lib/app/feature/cards_page/domain/cards.dart
@@ -34,86 +34,92 @@ class CollectibleCard {
 
 final appCardsCompleteList = [
   CollectibleCard(
-      id: 1, name: 'Eel', imagePath: 'assets/images/eel_card.png', rarity: 4),
+      id: 1, name: 'Eel', imagePath: 'assets/images/eel_card.jpeg', rarity: 4),
   CollectibleCard(
       id: 2,
       name: 'Eel Foil',
-      imagePath: 'assets/images/eel_card_f.png',
+      imagePath: 'assets/images/eel_card_f.jpeg',
       rarity: 5),
   CollectibleCard(
       id: 3,
       name: 'Flamingo',
-      imagePath: 'assets/images/flamingo_card.png',
+      imagePath: 'assets/images/flamingo_card.jpeg',
       rarity: 4),
   CollectibleCard(
       id: 4,
       name: 'Flamingo Foil',
-      imagePath: 'assets/images/flamingo_card_f.png',
+      imagePath: 'assets/images/flamingo_card_f.jpeg',
       rarity: 5),
   CollectibleCard(
-      id: 5, name: 'Duck', imagePath: 'assets/images/duck_card.png', rarity: 1),
+      id: 5,
+      name: 'Duck',
+      imagePath: 'assets/images/duck_card.jpeg',
+      rarity: 1),
   CollectibleCard(
-      id: 6, name: 'Fox', imagePath: 'assets/images/fox_card.png', rarity: 3),
+      id: 6, name: 'Fox', imagePath: 'assets/images/fox_card.jpeg', rarity: 3),
   CollectibleCard(
-      id: 7, name: 'Hawk', imagePath: 'assets/images/hawk_card.png', rarity: 3),
+      id: 7,
+      name: 'Hawk',
+      imagePath: 'assets/images/hawk_card.jpeg',
+      rarity: 3),
   CollectibleCard(
       id: 8,
       name: 'Heron',
-      imagePath: 'assets/images/heron_card.png',
+      imagePath: 'assets/images/heron_card.jpeg',
       rarity: 2),
   CollectibleCard(
       id: 9,
       name: 'Kingfisher',
-      imagePath: 'assets/images/kingfisher_card.png',
+      imagePath: 'assets/images/kingfisher_card.jpeg',
       rarity: 2),
   CollectibleCard(
       id: 10,
       name: 'Osprey',
-      imagePath: 'assets/images/osprey_card.png',
+      imagePath: 'assets/images/osprey_card.jpeg',
       rarity: 3),
   CollectibleCard(
       id: 11,
       name: 'Plover',
-      imagePath: 'assets/images/plover_card.png',
+      imagePath: 'assets/images/plover_card.jpeg',
       rarity: 2),
   CollectibleCard(
       id: 12,
       name: 'Nutria',
-      imagePath: 'assets/images/nutria_card.png',
+      imagePath: 'assets/images/nutria_card.jpeg',
       rarity: 1),
   CollectibleCard(
       id: 13,
       name: 'Frog',
-      imagePath: 'assets/images/frog_card.png',
+      imagePath: 'assets/images/frog_card.jpeg',
       rarity: 1),
   CollectibleCard(
       id: 14,
       name: 'Seagull',
-      imagePath: 'assets/images/seagull_card.png',
+      imagePath: 'assets/images/seagull_card.jpeg',
       rarity: 1),
   CollectibleCard(
       id: 15,
       name: 'Coot',
-      imagePath: 'assets/images/coot_card.png',
+      imagePath: 'assets/images/coot_card.jpeg',
       rarity: 1),
   CollectibleCard(
       id: 16,
       name: 'Pheasant',
-      imagePath: 'assets/images/pheasant_card.png',
+      imagePath: 'assets/images/pheasant_card.jpeg',
       rarity: 2),
   CollectibleCard(
       id: 17,
       name: 'Goby',
-      imagePath: 'assets/images/goby_card.png',
+      imagePath: 'assets/images/goby_card.jpeg',
       rarity: 2),
   CollectibleCard(
       id: 18,
       name: 'Mediterranean Killifish',
-      imagePath: 'assets/images/mediterranean_killifish_card.png',
+      imagePath: 'assets/images/mediterranean_killifish_card.jpeg',
       rarity: 2),
   CollectibleCard(
       id: 19,
       name: 'Three-Spined Stickleback',
-      imagePath: 'assets/images/three_spined_stickleback_card.png',
+      imagePath: 'assets/images/three_spined_stickleback_card.jpeg',
       rarity: 2),
 ];
