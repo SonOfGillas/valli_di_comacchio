bdc203b2c4895ca99c8369cfb8f2319bfdc8e693
Riccardo Gardenghi <riccardo.gardenghi@gellify.it>
2025-07-16 12:06:00 +0200

    add dev option button

--- lib/app/feature/profile/profile_page.dart
+++ lib/app/feature/profile/profile_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:valli_di_comacchio/app/shared/app_state/app_cubit.dart';
-import 'package:valli_di_comacchio/app/shared/components/appButton/glowing_button.dart';
-import 'package:valli_di_comacchio/app/shared/components/boarder_text/h3/h3.dart';
+import 'package:valli_di_comacchio/app/shared/app_state/app_state.dart';
 import 'package:valli_di_comacchio/app/shared/components/footer_nav_bar/footer_nav_bar.dart';
 import 'package:valli_di_comacchio/app/shared/components/valli_app_bar/valli_app_bar.dart';
+import 'package:valli_di_comacchio/app/shared/domain/data_sources/npc_data_source/npc_reset_data.dart';
+import 'package:valli_di_comacchio/app/shared/style/app_colors.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -12,25 +13,86 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppColors.palette_secondary,
         appBar: ValliAppBar(),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              H3('Profile'),
-              GlowingButton(
-                  text: 'enable dev mode',
-                  onPressed: () {
-                    context.read<AppCubit>().setDevMode(true);
-                  }),
-              GlowingButton(
-                  text: 'disable dev mode',
-                  onPressed: () {
-                    context.read<AppCubit>().setDevMode(false);
-                  })
-            ],
-          ),
+        body: Column(
+          children: [
+            DevModeWidget(),
+          ],
         ),
         bottomNavigationBar: FooterNavBar());
+  }
+}
+
+class DevModeWidget extends StatelessWidget {
+  const DevModeWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AppCubit, AppState>(
+      builder: (context, state) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Modalità Sviluppatore',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(width: 16),
+                Switch(
+                  value: state.devMode,
+                  onChanged: (bool value) {
+                    context.read<AppCubit>().setDevMode(value);
+                  },
+                ),
+              ],
+            ),
+            if (state.devMode) ...[
+              ElevatedButton(
+                child: Text('Ricevi 10 000 monete'),
+                onPressed: () {
+                  if (state.user == null) return;
+                  context.read<AppCubit>().updateUser(
+                        state.user?.copyWith(
+                          wealth: (state.user?.wealth ?? 0) + 10000,
+                        ),
+                      );
+                },
+              ),
+              ElevatedButton(
+                child: Text('Genera inventario casuale per l\'utente'),
+                onPressed: () {
+                  if (state.user == null) return;
+                  context.read<AppCubit>().updateUser(
+                        state.user?.copyWith(
+                          inventory: generateRndInventory(),
+                        ),
+                      );
+                },
+              ),
+              ElevatedButton(
+                child: Text('Resetta la collezione delle carte'),
+                onPressed: () {
+                  if (state.user == null) return;
+                  context.read<AppCubit>().updateUser(
+                        state.user?.copyWith(
+                          cardCollection: const {},
+                        ),
+                      );
+                },
+              )
+            ],
+          ],
+        );
+      },
+    );
   }
 }
--- lib/app/shared/domain/data_sources/user_data_source/app_storage_user_data_source.dart
+++ lib/app/shared/domain/data_sources/user_data_source/app_storage_user_data_source.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:valli_di_comacchio/app/feature/trade/domain/entities/need_level.dart';
 import 'package:valli_di_comacchio/app/feature/trade/domain/entities/production_level.dart';
+import 'package:valli_di_comacchio/app/shared/domain/data_sources/npc_data_source/npc_reset_data.dart';
 import 'package:valli_di_comacchio/app/shared/domain/data_sources/user_data_source/user_data_source.dart';
 import 'package:valli_di_comacchio/app/shared/domain/entities/app_user.dart';
 import 'package:valli_di_comacchio/app/shared/domain/entities/trade_resource_inventory.dart';
@@ -51,17 +52,7 @@ class AppStorageUserDataSource extends LocalUserDataSource {
       id: email,
       email: email,
       username: username,
-      inventory: tradeResourcesList
-          .map(
-            (resource) => TradeResourceInventory(
-              tradeResource: resource,
-              defaultProductionLevel: ProductionLevel.notProduced,
-              defaultNeedLevel: NeedLevel.notInterested,
-              storage: 0,
-              needs: 0,
-            ),
-          )
-          .toList(),
+      inventory: generateRndInventory(),
       cardCollection: const {},
       isGuest: isGuest,
       wealth: 10000,
