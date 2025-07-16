12aef375426e6bbc564c7720551c3dc25375a366
Riccardo Gardenghi <riccardo.gardenghi@gellify.it>
2025-07-16 14:37:45 +0200

    add logout to profile page

--- lib/app/feature/profile/profile_page.dart
+++ lib/app/feature/profile/profile_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:valli_di_comacchio/app/feature/trade/presentation/components/trade_resource_element.dart';
 import 'package:valli_di_comacchio/app/shared/app_state/app_cubit.dart';
 import 'package:valli_di_comacchio/app/shared/app_state/app_state.dart';
 import 'package:valli_di_comacchio/app/shared/components/boarder_text/h2/h2.dart';
+import 'package:valli_di_comacchio/app/shared/components/boarder_text/h3/h3.dart';
 import 'package:valli_di_comacchio/app/shared/components/boarder_text/labelText.dart/label_text.dart';
 import 'package:valli_di_comacchio/app/shared/components/footer_nav_bar/footer_nav_bar.dart';
 import 'package:valli_di_comacchio/app/shared/components/valli_app_bar/valli_app_bar.dart';
+import 'package:valli_di_comacchio/app/shared/core/routes/routes_paths.dart';
 import 'package:valli_di_comacchio/app/shared/domain/data_sources/npc_data_source/npc_reset_data.dart';
 import 'package:valli_di_comacchio/app/shared/style/app_colors.dart';
 
@@ -22,6 +25,32 @@ class ProfilePage extends StatelessWidget {
           child: Column(
             children: [
               DevModeWidget(),
+              //logged user info and logout button
+              BlocBuilder<AppCubit, AppState>(
+                builder: (context, state) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        if (state.user?.isGuest ?? false)
+                          H2('Utente:    Ospite'),
+                        if (state.user != null &&
+                            !(state.user?.isGuest ?? false))
+                          H2('Utente:    ${state.user?.username ?? 'Sconosciuto'}'),
+                        SizedBox(width: 16),
+                        IconButton(
+                          icon: Icon(Icons.logout,
+                              color: AppColors.palette_primary),
+                          onPressed: () {
+                            context.read<AppCubit>().logout();
+                            context.go(RoutesPaths.auth);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
               SizedBox(height: 8),
               H2('Il tuo inventario'),
               SizedBox(height: 4),
